7148f6389c3cac5ca0389cdbda2d9455d21906fb
goswamiarpit475 <goswami.arpit475@gmail.com>
2023-01-05 10:21:24 +0530

    done

--- GenerateThumbNails/Form1.Designer.cs
+++ GenerateThumbNails/Form1.Designer.cs
@@ -29,33 +29,58 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(95, 116);
+            this.button1.Location = new System.Drawing.Point(137, 203);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(157, 55);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Generate Thumbnails";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Processing files: ";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(198, 72);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(447, 302);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
--- GenerateThumbNails/Form1.cs
+++ GenerateThumbNails/Form1.cs
@@ -16,6 +16,10 @@ namespace GenerateThumbNails
 {
     public partial class Form1 : Form
     {
+         string ffmpegPath = "";
+         string thumbPath = "";
+         string inputfilePath = "";
+        string vidMainDir = "";
         public Form1()
         {
             InitializeComponent();
@@ -24,23 +28,42 @@ namespace GenerateThumbNails
         private void button1_Click(object sender, EventArgs e)
         {
             //VideoInfo v= FFMpegSharp.VideoInfo.FromPath("");
-           /* string inputFile = @"C:\\Users\\ahc\\Downloads\\SoundID\\sample_video.mp4";
-            FileInfo output = new FileInfo("C:\\Users\\ahc\\Downloads\\SoundID\\t.png");
+            /* string inputFile = @"C:\\Users\\ahc\\Downloads\\SoundID\\sample_video.mp4";
+             FileInfo output = new FileInfo("C:\\Users\\ahc\\Downloads\\SoundID\\t.png");
 
-            var video = VideoInfo.FromPath(inputFile);
+             var video = VideoInfo.FromPath(inputFile);
 
-            new FFMpeg()
-                .Snapshot(
-                    video,
-                    output,
-                    new Size(200, 400),
-                    TimeSpan.FromSeconds(600)
-                );*/
-            GetThumbnail("", "");
+             new FFMpeg()
+                 .Snapshot(
+                     video,
+                     output,
+                     new Size(200, 400),
+                     TimeSpan.FromSeconds(600)
+                 );*/
+            List<string> vidPaths = loadInputFile();
+            int i = 0;
+            foreach (string s in vidPaths)
+            {
+                i++;
+                string vidPath = "";
+                string thumbTime = "";
+                string thumbNail = "";
+                string[] x = s.Split('_');
+                vidPath = vidMainDir + "\\" + x[0]+".mp4";
+                thumbTime = x[1].Replace("\r", "");
+                thumbNail = thumbPath+"\\"+x[0].Replace('/', '_').Replace("\r","")+".png";
+                GetThumbnail(vidPath, thumbNail, thumbTime);
+                lblStatus.Text = i.ToString() + " of " + vidPaths.Count().ToString()+"...";
+                if (i >3 )
+                {
+                    break;
+                }
+            }
+            lblStatus.Text = "Complete";
         }
-        public static void GetThumbnail(string video, string thumbnail)
+        public void GetThumbnail(string video, string thumbnail,string thumbtime)
         {
-            var cmd = "C:\\ffmpeg\\bin\\ffmpeg  -i C:\\Users\\ahc\\Downloads\\SoundID\\sample_video.mp4 -ss 00:10:00.000 -vf scale=320:320:force_original_aspect_ratio=decrease -vframes 1 C:\\Users\\ahc\\Downloads\\SoundID\\output.png";
+            string cmd = ffmpegPath+" -i \""+video+"\" -ss "+thumbtime+" -vf scale=320:320:force_original_aspect_ratio=decrease -vframes 1 \""+thumbnail+"\"";
 
             var startInfo = new ProcessStartInfo
             {
@@ -65,6 +88,23 @@ namespace GenerateThumbNails
         {
             var ms = new MemoryStream(File.ReadAllBytes(path));
             return (Bitmap)Image.FromStream(ms);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string currDirPath = Directory.GetCurrentDirectory();
+            ffmpegPath = currDirPath + "\\FFMPEG\\bin\\x64\\ffmpeg";
+            inputfilePath = currDirPath + "\\video_data\\input.txt";
+            thumbPath = currDirPath + "\\video_data\\thumbs";
+            vidMainDir = currDirPath + "\\video_data";
+
+
+        }
+        private List<string> loadInputFile()
+        {
+            string inputTxt = File.ReadAllText(inputfilePath);
+            List<string> videoPath = inputTxt.Split('\n').ToList();
+            return videoPath;
         }
     }
 }
